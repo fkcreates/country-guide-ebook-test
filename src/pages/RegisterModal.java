1bcd04d5fb8849bcfd42883010e17bbae22bdebf
Márton Tévald <marton.tevald@gmail.com>
2019-11-13 22:03:29 +0100

    findby usernameField

--- src/pages/RegisterModal.java
+++ src/pages/RegisterModal.java
@@ -1,4 +1,11 @@
 package pages;
 
-public class RegisterModal {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class RegisterModal extends BasePage {
+
+    @FindBy(xpath = "//input[@name='userName']")
+    private WebElement userNameField;
+
 }
