00496d3764b4b58843d1be6f7ba26708ecd47f0e
Márton Tévald <marton.tevald@gmail.com>
2019-11-13 11:49:57 +0100

    open register modal test

--- src/tests/TestRegistration.java
+++ src/tests/TestRegistration.java
@@ -1,4 +1,14 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+import pages.HomePage;
+
 public class TestRegistration {
+
+    @Test
+    void testRegistration(){
+        HomePage homePage = new HomePage();
+        homePage.openRegisterModal();
+
+    }
 }
