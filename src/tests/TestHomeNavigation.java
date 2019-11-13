c2fa24e965321802b9fd53d236c5aa1a84b77d35
Fruzsina Kiss <fruzsinakiss1@gmail.com>
2019-11-13 14:48:39 +0100

    trial test is running

--- src/tests/TestHomeNavigation.java
+++ src/tests/TestHomeNavigation.java
@@ -1,4 +1,15 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import util.Driver;
+
 public class TestHomeNavigation {
+    WebDriver driver = Driver.getInstance();
+
+
+    @Test
+    public void test(){
+        driver.get("http://localhost:3000");
+    }
 }
